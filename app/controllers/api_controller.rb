cd527deacfc466c3214368e41638f8e44c506021
Bosco So <bosco@sfruby.info>
2014-09-18 15:24:25 -0400

    removed extraneous commas in API status

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,6 +1,6 @@
 class ApiController < ApplicationController
   def check_fraud
-    random_status = %w(found, not_found, non_compliant)[rand 3]
+    random_status = %w(found not_found non_compliant)[rand 3]
     render json: {status: random_status}
   end
 end
