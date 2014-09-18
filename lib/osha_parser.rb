680a6904f2450060469e2392e170551ef1576946
Tyrone Grandison <tgrandison@gmail.com>
2014-09-18 14:24:27 -0400

    full record from xml

--- lib/osha_parser.rb
+++ lib/osha_parser.rb
@@ -20,6 +20,14 @@ class OshaParser
           site_city: prop.xpath('d:site_city').text,
           site_state: prop.xpath('d:site_state').text,
           osha_violation_indicator: 'true' == prop.xpath('d:osha_violation_indicator').text,
+          site_address:  prop.xpath('d:site_address').text,
+          naics_code: prop.xpath('d:naics_code').text,
+          insp_type: prop.xpath('d:insp_type:').text,
+          open_date: prop.xpath('d:open_date').text,
+          total_current_penalty: prop.xpath('d:total_current_penalty').text,
+          us_violations: prop.xpath('d:us_violations').text,
+          total_violations: prop.xpath('d:total_violations').text,
+          load_dt: prop.xpath('d:load_dt').text,
       }
     end
   end
