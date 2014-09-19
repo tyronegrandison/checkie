e86498ee23c898741d5a118128b05fb0a546a771
Julia Winn <unfinished.art@gmail.com>
2014-09-19 12:21:55 -0400

    working better now, says name of vendor

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,16 +1,15 @@
 $( document ).ready(function() {
 	$("#searchForm").submit(function(event) {
           /* stop form from submitting normally */
-          event.preventDefault();
-          $('.response').hide();
+        event.preventDefault();
+        $('.response').hide();
 
-          localStorage.clear();
-          localStorage.setItem("name", $("#name")[0].value);
-  				var promise = jQuery.ajax(
-  				{
-  					url: "/api/am_i_a_fraud",
-  					data: {name: $("#name")[0].value, city: $("#city")[0].value, state: $("#state")[0].value}
-  				});
+        var name = toTitleCase($("#name")[0].value);
+		var promise = jQuery.ajax(
+		{
+			url: "/api/am_i_a_fraud",
+			data: {name: $("#name")[0].value, city: $("#city")[0].value, state: $("#state")[0].value}
+		});
 
           promise.success(
 	          function(data) {  
@@ -21,15 +20,18 @@ $( document ).ready(function() {
 	          	var response_text = document.createElement('h3');
 	            if ('found' == data.status) {
 	              $('#found').show();
-	              $('#response_text').append('<h3>Good news!</h3><h3>This is a valid vendor, pay away!</h3>');
+	              var response_text = '<h3>Good news!</h3><h3><span style="color: #5A0000">' + name + '</span> is a valid vendor, pay away!</h3>'
+	              $('#response_text').append(response_text);
 	            }
 	            else if ('not_found' == data.status) {
 	              	$('#not_found').show();
-	              	$('#response_text').append("<h3>Hmmm... We couldn't find them in the system.</h3><h3>Double check with someone before paying then!</h3>");
+	              	var response_text = "<h3>Hmmm... We couldn\'t find <span style='color: #5A0000'>" + name + "</span> in the system.</h3><h3>Double check with someone before paying then!</h3>";
+	              	$('#response_text').append(response_text);
 	            }
 	            else if ('non_compliant' == data.status) {
 	              	$('#non_compliant').show();
-	              	$('#response_text').append("<h3>This vendor is non-compliant!</h3><h3>Don't pay them</h3>");
+	              	var response_text = "<h3><span style='color: #5A0000'>" + name + "</span> is non-compliant!</h3><h3>Don't pay them</h3>";
+	              	$('#response_text').append(response_text);
 	            	}
 	            $('#summary').show();
 	           }
@@ -44,4 +46,9 @@ function go_back() {
 	$('.response').hide();
 	var form = document.getElementById("searchForm");
 	form.reset()
+}
+
+function toTitleCase(str)
+{
+    return str.replace(/\w\S*/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
 }
